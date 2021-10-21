29c161f5934c1652f86b3eaff44fc79f82c31337
cyklop123 <irekbiela@gmail.com>
2021-10-21 12:15:28 +0200

    Add db saving

--- main.py
+++ main.py
@@ -5,31 +5,75 @@ from bs4 import BeautifulSoup
 
 INTERVAL_TIME_SECONDS = 600 # 10 min
 PAGE_START = 1
-PAGE_END = 10 #docelowo 1000
+PAGE_END = 100 #docelowo 1000
 
 def main():
     signal.signal(signal.SIGTERM, programTermination)
     signal.signal(signal.SIGINT, programTermination)
 
+    db = initMongo()
+    movies = db['movies']
+
     while True:
         try:
-            get_data(url = 'https://www.filmweb.pl/serials/search?orderBy=rate&descending=true', pages = range(PAGE_START, PAGE_END+1))
+            print("Starting data collection")
+            start = time.time()
+            get_data(url = 'https://www.filmweb.pl/serials/search?orderBy=rate&descending=true', pages = range(PAGE_START, PAGE_END+1), db=movies)
+            end = time.time()
+            print("Finishing data collection")
+            print("Elapsed time",end-start)
             print("Next iteration in", INTERVAL_TIME_SECONDS, 'seconds\n')
             time.sleep(INTERVAL_TIME_SECONDS)
         except Termination:
             print("\nProgram terminated.")
             break
 
-def saveData(data):
-    pass #ma zapisywać pojedynczy film do bazy
+def initMongo():
+    try:
+        client = pymongo.MongoClient("mongodb://minadzd:minadzd@localhost:27017/filmweb")
+        db = client['filmweb']
+        db.list_collection_names()
+        print("Database connection established")
+        return db
+    except:
+        print("Database connection error")
+        exit()
 
-def checkIfFilmExist(id):
-    return False # ma zwracać true jeżeli film istnieje w bazie lub false jeżeli nie istnieje
+def saveData(data, db):
+    movie = {
+        'filmweb-id': data['id'],
+        'name': data['name'],
+        'nameOrig': data['nameOrig'],
+        'year': data['year'],
+        'rate': data['rate'],
+        'rateCount': data['rateCount']
+    }
+    if data['wantToSee']:
+        movie['wantToSee'] = data['wantToSee']
+    if data['releaseDate']:
+        movie['releaseDate'] = data['releaseDate']
+    if data['episodeDuration']:
+        movie['episodeDuration'] = data['episodeDuration']
+    if data['decription']:
+        movie['decription'] = data['decription']
+    movie['genres'] = list(data['genres'])
+    movie['countries'] = list(data['countries'])
+    movie['directors'] = list(data['directors'])
+    movie['cast'] = list(data['cast'])
 
-def get_data(url, pages):
+    try:
+        db.insert_one(movie)
+    except:
+        print("Movie insertion error")
+        exit()
+
+def checkIfFilmExist(id, name, db):
+    return True if db.count_documents({'filmweb-id': id, 'name': name}) > 0 else False
+
+def get_data(url, pages, db):
     collected = 0
     new = 0
-    print("Collecting page: 0 /",pages[-1], end='')
+    print("Collecting page:", pages[0], "/", pages[-1], end='')
     for page in pages:
         r = requests.get(url+'&page='+str(page))
         print("\rCollecting page:", page, '/', pages[-1], end='')
@@ -39,56 +83,71 @@ def get_data(url, pages):
 
             for film in films:
                 data = extractData(film)
-                if ~checkIfFilmExist(data[0]):
-                    saveData(data)
+                if not checkIfFilmExist(data['id'], data['name'], db):
+                    saveData(data, db)
                     new += 1
                 collected += 1
         else:
-            break
             print(r.headers)
+            break
     print()
     print("Collected", collected, "films.", new, "new.")
 
 def extractData(film):
-    id = film.find("div", class_="poster--auto")["data-film-id"]
-    name = film.find("h2", class_="filmPreview__title").text
+    id = film.find("div", class_="poster--auto")["data-film-id"].strip()
+    name = film.find("h2", class_="filmPreview__title").text.strip()
     nameOrigTag = film.find("div", class_="filmPreview__originalTitle")
-    nameOrig = nameOrigTag.text if nameOrigTag != None else name
-    year = film.find("div", class_="filmPreview__year").text
+    nameOrig = nameOrigTag.text.strip() if nameOrigTag != None else name
+    year = film.find("div", class_="filmPreview__year").text.strip()
     rateBoxTag = film.find("div", class_="filmPreview__rateBox")
     rate = rateBoxTag.get('data-rate')
     rateCount = rateBoxTag.get('data-count')
     wantToSeeTag = film.find('div', class_="filmPreview__wantToSee")
     wantToSee = wantToSeeTag.get('data-wanna')
     realeaseDateTag = film.find("div", class_="filmPreview__release")
-    realeaseDate = realeaseDateTag.get("data-release")
+    releaseDate = realeaseDateTag.get("data-release")
     episodeDurationTag = film.find("div", class_="filmPreview__filmTime")
     episodeDuration = episodeDurationTag.get('data-duration') if episodeDurationTag else None
     descriptionTag = film.find("div", class_="filmPreview__description")
-    decription = descriptionTag.text if descriptionTag else None
+    decription = descriptionTag.text.strip() if descriptionTag else None
 
     genresTag = film.find("div", class_="filmPreview__info--genres")
     if genresTag:
-        genres = [genre.text for genre in genresTag.find_all('a')]
+        genres = (genre.text.strip() for genre in genresTag.find_all('a'))
     else:
-        genres = []
+        genres = ()
     countriesTag = film.find("div", class_="filmPreview__info--countries")
     if countriesTag:
-        countries = [country.text for country in countriesTag.find_all('a')]
+        countries = (country.text.strip() for country in countriesTag.find_all('a'))
     else:
-        countries = []
+        countries = ()
     directorsTag = film.find("div", class_="filmPreview__info--directors")
     if directorsTag:
-        directors = [director.text for director in directorsTag.find_all('a')]
+        directors = (director.text.strip() for director in directorsTag.find_all('a'))
     else:
-        directors = []
+        directors = ()
     castTag = film.find("div", class_="filmPreview__info--cast")
     if castTag:
-        cast = [actor.text for actor in castTag.find_all('a')]
+        cast = (actor.text.strip() for actor in castTag.find_all('a'))
     else:
-        cast = []
+        cast = ()
 
-    return [ id, name, nameOrig, year, rate, rateCount, wantToSee, realeaseDate, episodeDuration, decription, genres, countries, directors, cast ]
+    return {
+        "id": id,
+        "name": name,
+        "nameOrig": nameOrig,
+        "year": year,
+        "rate": rate,
+        "rateCount": rateCount,
+        "wantToSee": wantToSee,
+        "releaseDate": releaseDate,
+        "episodeDuration": episodeDuration,
+        "decription": decription,
+        "genres": genres,
+        "countries": countries,
+        "directors": directors,
+        "cast": cast
+    }
 
 class Termination(Exception):
     pass
